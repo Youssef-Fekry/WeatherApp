c9eedea9c3c61cecb47abfdc802d80128f5ff068
Youssef Fekry <yousseffekry14@gmail.com>
2024-03-31 13:52:20 +0200

    update

--- lib/models/get_weather_theme.dart
+++ lib/models/get_weather_theme.dart
@@ -1,81 +1,81 @@
 import 'package:flutter/material.dart';
 
 MaterialColor getThemeColor(String? condition) {
-  if (condition == 'sunny') {
+  if (condition == 'Sunny') {
     return Colors.orange;
-  } else if (condition == 'partly cloudy') {
+  } else if (condition == 'Partly cloudy') {
     return Colors.yellow;
-  } else if (condition == 'cloudy') {
+  } else if (condition == 'Cloudy') {
     return Colors.grey;
-  } else if (condition == 'overcast') {
+  } else if (condition == 'Overcast') {
     return Colors.blueGrey;
   } else if (condition == 'mist') {
     return Colors.lightBlue;
-  } else if (condition == 'patchy rain possible') {
+  } else if (condition == 'Patchy rain possible') {
     return Colors.lightGreen;
-  } else if (condition == 'patchy snow possible' ||
-      condition == 'blowing snow' ||
-      condition == 'blizzard' ||
-      condition == 'patchy sleet possible' ||
-      condition == 'patchy freezing drizzle possible') {
+  } else if (condition == 'Patchy snow possible' ||
+      condition == 'Blowing snow' ||
+      condition == 'Blizzard' ||
+      condition == 'Patchy sleet possible' ||
+      condition == 'Patchy freezing drizzle possible') {
     return Colors.lightBlue;
-  } else if (condition == 'thundery outbreaks possible') {
+  } else if (condition == 'Thundery outbreaks possible') {
     return Colors.deepPurple;
-  } else if (condition == 'fog') {
+  } else if (condition == 'Fog') {
     return Colors.grey;
-  } else if (condition == 'freezing fog' ||
-      condition == 'patchy light drizzle' ||
-      condition == 'light drizzle' ||
-      condition == 'freezing drizzle') {
+  } else if (condition == 'Freezing fog' ||
+      condition == 'Patchy light drizzle' ||
+      condition == 'Light drizzle' ||
+      condition == 'Freezing drizzle') {
     return Colors.lightBlue;
-  } else if (condition == 'heavy freezing drizzle') {
+  } else if (condition == 'Heavy freezing drizzle') {
     return Colors.lightBlue;
-  } else if (condition == 'patchy light rain') {
+  } else if (condition == 'Patchy light rain') {
     return Colors.lightGreen;
-  } else if (condition == 'light rain') {
+  } else if (condition == 'Light rain') {
     return Colors.lightGreen;
-  } else if (condition == 'moderate rain at times') {
+  } else if (condition == 'Moderate rain at times') {
     return Colors.green;
-  } else if (condition == 'moderate rain') {
+  } else if (condition == 'Moderate rain') {
     return Colors.green;
-  } else if (condition == 'heavy rain at times') {
+  } else if (condition == 'Heavy rain at times') {
     return Colors.green;
-  } else if (condition == 'heavy rain') {
+  } else if (condition == 'Heavy rain') {
     return Colors.green;
-  } else if (condition == 'light freezing rain' ||
-      condition == 'moderate or heavy freezing rain' ||
-      condition == 'light sleet' ||
-      condition == 'moderate or heavy sleet' ||
-      condition == 'patchy light snow' ||
-      condition == 'light snow' ||
-      condition == 'patchy moderate snow' ||
-      condition == 'moderate snow' ||
-      condition == 'patchy heavy snow' ||
-      condition == 'heavy snow' ||
-      condition == 'ice pellets') {
+  } else if (condition == 'Light freezing rain' ||
+      condition == 'Moderate or heavy freezing rain' ||
+      condition == 'Light sleet' ||
+      condition == 'Moderate or heavy sleet' ||
+      condition == 'Patchy light snow' ||
+      condition == 'Light snow' ||
+      condition == 'Patchy moderate snow' ||
+      condition == 'Moderate snow' ||
+      condition == 'Patchy heavy snow' ||
+      condition == 'Heavy snow' ||
+      condition == 'Ice pellets') {
     return Colors.lightBlue;
-  } else if (condition == 'light rain shower') {
+  } else if (condition == 'Light rain shower') {
     return Colors.lightGreen;
-  } else if (condition == 'moderate or heavy rain shower' ||
-      condition == 'torrential rain shower') {
+  } else if (condition == 'Moderate or heavy rain shower' ||
+      condition == 'Torrential rain shower') {
     return Colors.green;
-  } else if (condition == 'light sleet showers' ||
-      condition == 'moderate or heavy sleet showers' ||
-      condition == 'light snow showers' ||
-      condition == 'moderate or heavy snow showers' ||
-      condition == 'light showers of ice pellets' ||
-      condition == 'moderate or heavy showers of ice pellets') {
+  } else if (condition == 'Light sleet showers' ||
+      condition == 'Moderate or heavy sleet showers' ||
+      condition == 'Light snow showers' ||
+      condition == 'Moderate or heavy snow showers' ||
+      condition == 'Light showers of ice pellets' ||
+      condition == 'Moderate or heavy showers of ice pellets') {
     return Colors.lightBlue;
-  } else if (condition == 'patchy light rain with thunder' ||
-      condition == 'moderate or heavy rain with thunder' ||
-      condition == 'patchy light snow with thunder' ||
-      condition == 'moderate or heavy snow with thunder') {
+  } else if (condition == 'Patchy light rain with thunder' ||
+      condition == 'Moderate or heavy rain with thunder' ||
+      condition == 'Patchy light snow with thunder' ||
+      condition == 'Moderate or heavy snow with thunder') {
     return Colors.deepPurple;
   }
   // else if (condition == null) {
   //   return Colors.blue;
   // }
   else {
-    return Colors.orange;
+    return Colors.blue;
   }
 }
